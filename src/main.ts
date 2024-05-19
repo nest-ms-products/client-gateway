e89c89960b9cf4ee79040089d9f0e118a58ae5d6
Jean Osco <willjean29@gmail.com>
2024-05-19 13:31:02 -0500

    init: setup project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+  logger.log(`Gateway started running on${envs.port}`);
+}
+bootstrap();
