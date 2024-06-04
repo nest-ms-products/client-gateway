189014edd4780a0183d1716cd97ad766e5a8680e
Jean Osco <willjean29@gmail.com>
2024-06-04 11:15:43 -0500

    feat: add transport module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule, OrdersModule],
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NatsService } from 'src/common/enums/services.enum';
-import { envs } from '../config/envs';
 import { ProductsController } from './products.controller';
+import { NatsModule } from 'src/transports/nats.module';
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: NatsService,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [ProductsController],
   providers: [],
 })
