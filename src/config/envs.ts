a23dea397b95b691b58965fd0b547667b00725d5
Jean Osco <willjean29@gmail.com>
2024-06-05 11:59:07 -0500

    chore: add docker file

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,20 +2,12 @@ import 'dotenv/config';
 import * as joi from 'joi';
 interface EnVars {
   PORT: number;
-  MS_PRODUCTS_PORT: number;
-  MS_PRODUCTS_HOST: string;
-  MS_ORDERS_PORT: number;
-  MS_ORDERS_HOST: string;
   NATS_SERVERS: string[];
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
-    MS_PRODUCTS_PORT: joi.number().required(),
-    MS_PRODUCTS_HOST: joi.string().required(),
-    MS_ORDERS_PORT: joi.number().required(),
-    MS_ORDERS_HOST: joi.string().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
@@ -32,9 +24,5 @@ const enVars: EnVars = value;
 
 export const envs = {
   port: enVars.PORT,
-  msProductsPort: enVars.MS_PRODUCTS_PORT,
-  msProductsHost: enVars.MS_PRODUCTS_HOST,
-  msOrdersPort: enVars.MS_ORDERS_PORT,
-  msOrdersHost: enVars.MS_ORDERS_HOST,
   natsServers: enVars.NATS_SERVERS,
 };
