702f7ea2a8f4866fcc7b62a072aedb5e762fdc85
Jean Osco <willjean29@gmail.com>
2024-06-17 17:29:11 -0500

    feat: implement verify service

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,13 +1,19 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+import { AuthMessages } from 'src/common/enums/messages-tcp.enum';
+import { NatsService } from 'src/common/enums/services.enum';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(@Inject(NatsService) private readonly client: ClientProxy) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
@@ -17,12 +23,11 @@ export class AuthGuard implements CanActivate {
     try {
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
-      request.user = {
-        id: '454554',
-        name: 'John Doe',
-        email: 'john@email.com',
-      };
-      request.token = token;
+      const { user, token: newToken } = await firstValueFrom(
+        this.client.send(AuthMessages.Verify, token),
+      );
+      request.user = user;
+      request.token = newToken;
     } catch {
       throw new UnauthorizedException();
     }
